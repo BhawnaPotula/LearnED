ec129dd1513803c41e683ddfc677a82017010f55
anshubaloria123@gmail.com <you@example.com>
2021-07-03 19:37:01 +0530

    Added who we are

---
+++ src/components/Slidebar.js
@@ -0,0 +1,52 @@
+export const Slidebar=
+    [
+        {
+            image:"https://www.kickincustoms.com/wp-content/uploads/2016/06/20.jpg",
+            title:"CEO",
+            body:" Lorem ipsum dolor sit amet, consectetur adipisci hdfvh hdfshdsvf ndfgudsfj ndgfuvdsjfv ngdfugdsufvfdh ndgf7dselit,  mollit anim id est laborum. »",
+        },
+        
+        {
+            image:"https://www.kickincustoms.com/wp-content/uploads/2016/06/20.jpg",
+            title:"CEO",
+            body:" Lorem ipsum dolor sit amet, consectetur adipisci hdfvh hdfshdsvf ndfgudsfj ndgfuvdsjfv ngdfugdsufvfdh ndgf7dselit,  mollit anim id est laborum. »",
+
+        }
+        ,
+        
+        {
+            image:"https://www.kickincustoms.com/wp-content/uploads/2016/06/20.jpg",
+            title:"CEO",
+            body:" Lorem ipsum dolor sit amet, consectetur adipisci hdfvh hdfshdsvf ndfgudsfj ndgfuvdsjfv ngdfugdsufvfdh ndgf7dselit,  mollit anim id est laborum. »",
+        },
+        
+        {
+            image:"https://www.kickincustoms.com/wp-content/uploads/2016/06/20.jpg",
+            title:"CEO",
+            body:" Lorem ipsum dolor sit amet, consectetur adipisci hdfvh hdfshdsvf ndfgudsfj ndgfuvdsjfv ngdfugdsufvfdh ndgf7dselit,  mollit anim id est laborum. »",
+        }
+        ,
+        
+        {
+            image:"https://www.kickincustoms.com/wp-content/uploads/2016/06/20.jpg",
+            title:"CEO",
+            body:" Lorem ipsum dolor sit amet, consectetur adipisci hdfvh hdfshdsvf ndfgudsfj ndgfuvdsjfv ngdfugdsufvfdh ndgf7dselit,  mollit anim id est laborum. »",
+        },
+        {
+            image:"https://www.kickincustoms.com/wp-content/uploads/2016/06/20.jpg",
+            title:"CEO",
+            body:" Lorem ipsum dolor sit amet, consectetur adipisci hdfvh hdfshdsvf ndfgudsfj ndgfuvdsjfv ngdfugdsufvfdh ndgf7dselit,  mollit anim id est laborum. »",
+        },
+        
+        {
+            image:"https://www.kickincustoms.com/wp-content/uploads/2016/06/20.jpg",
+            title:"CEO",
+            body:" Lorem ipsum dolor sit amet, consectetur adipisci hdfvh hdfshdsvf ndfgudsfj ndgfuvdsjfv ngdfugdsufvfdh ndgf7dselit,  mollit anim id est laborum. »",
+        },
+        
+        {
+            image:"https://www.kickincustoms.com/wp-content/uploads/2016/06/20.jpg",
+            title:"CEO",
+            body:" Lorem ipsum dolor sit amet, consectetur adipisci hdfvh hdfshdsvf ndfgudsfj ndgfuvdsjfv ngdfugdsufvfdh ndgf7dselit,  mollit anim id est laborum. »",
+        }
+]; 

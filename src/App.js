17da0ea8769529ced7d302c3ea51a8d7408d7cbe
rohankar7 <rohankar365@gmail.com>
2021-07-04 09:20:45 +0530

    Updated Landing Page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import MainContent from './components/Content/MainContent';
+import LandingPage from './components/LandingPage/LandingPage';
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <MainContent/>
+      <BrowserRouter>
+        <Switch>
+          <Route path='/' exact component={MainContent} />
+          <Route path='/landing-page' component={LandingPage} />
+        </Switch>
+      </BrowserRouter>
+      {/* <MainContent/> */}
       <Footer/>
     </div>
   );

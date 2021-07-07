3258655691c4326e43aff531fa890462ff0b6d50
anshubaloria123@gmail.com <you@example.com>
2021-07-07 17:01:01 +0530

    Addde  carousel

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import './App.css';
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
-
+ import ImageSlider from './components/ImageSlider';
 import Aboutus from './components/Aboutus';
 import Team from './components/Team';
 function App() {
   return (
     <div className="App">
       <Header />
-      <Aboutus/>
-      <Team/>
-      <Footer/>
-    </div>
+      <Aboutus/><ImageSlider/></div>
   );
 }
 

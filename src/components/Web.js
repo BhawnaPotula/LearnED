2a81dce2b84072091e4e1460b8e6a8d40cd19466
anshubaloria123@gmail.com <you@example.com>
2021-07-14 13:28:39 +0530

    Cards section added

--- src/components/Web.js
+++ src/components/Web.js
@@ -1,14 +1,15 @@
 import React ,{useState}from 'react'
 import Cardss from './Cardss'
 import "./Web.css"
+import Webteam from './our/Webteam';
 import { AppBar,Toolbar ,Button} from '@material-ui/core';
 const Web = () => {
     const[active,setactive]=useState("Firstcard");
     return (
         <div >
-            <h1 style={{textAlign:"center"}}>OUR TEAM</h1>
+            <h1 style={{textAlign:"center",fontFamily:"fantasy"}}>OUR TEAM</h1>
             <div className="btn">
-            <AppBar position="static "style={{background:"lightgray"}}>
+            <AppBar position="static "style={{background:"lightgray",}}>
                 <Toolbar style={{ display:"flex",justifyContent:"space-between"}} >
                     <Button onClick={()=>setactive("Firstcard")} style={{color:"red" ,font:"21em"}}>web</Button >
                     <Button onClick={()=>setactive("Secondcard")} style={{color:"red"}}>back</Button>
@@ -18,7 +19,7 @@ const Web = () => {
             </AppBar>
             </div>
             <div className="n">
-                {active==="Firstcard"&&<Cardss title={<h1 style={{background:"lightblue",height:"400px",textAlign:"center"}}>webteam </h1>}/>}
+                {active==="Firstcard"&&<Cardss title={<Webteam/>}/>}
                 {active==="Secondcard"&&<Cardss title={<h1 style={{background:"lightblue",height:"400px",textAlign:"center"}}>back-team </h1>}/>}
                 {active==="Thirdcard"&&<Cardss title={<h1 style={{background:"lightblue",height:"400px",textAlign:"center"}}>Contentteam </h1>}/>}
                 {active==="fourthcard"&&<Cardss title={<h1 style={{background:"lightblue",height:"400px",textAlign:"center"}}>others </h1>}/>}
---
+++ src/components/our/Webteam.js
@@ -0,0 +1,244 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import logo from "../images/Ashish.jpg"
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 300,
+    margin:20,
+  },
+  media: {
+    height: 150,
+  },
+});
+
+export default function Webteam () {
+  const classes = useStyles();
+
+  return (
+    <>
+    <div style={{display:"flex",flexWrap:"wrap",justifyContent:"space-around"}}>
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="{logo}"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            WEB DEVELOPER 
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry.  
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      
+    </Card>
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="{logo}"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            WEB DEVELOPER 
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry.  
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      
+    </Card>
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="{logo}"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            WEB DEVELOPER 
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry.  
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      
+    </Card>
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="{logo}"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            WEB DEVELOPER 
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry.  
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      
+    </Card>
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="{logo}"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            WEB DEVELOPER 
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry.  
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      
+    </Card>
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="{logo}"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            WEB DEVELOPER 
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry.  
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      
+    </Card>
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="{logo}"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            WEB DEVELOPER 
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry.  
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      
+    </Card>
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="{logo}"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            WEB DEVELOPER 
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry.  
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      
+    </Card>
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="{logo}"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            WEB DEVELOPER 
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry.  
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      
+    </Card>
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="{logo}"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            WEB DEVELOPER 
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry.  
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      
+    </Card>
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="{logo}"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            WEB DEVELOPER 
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry.  
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      
+    </Card>
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="{logo}"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            WEB DEVELOPER 
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry.  
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      
+    </Card>
+    </div>
+    </>
+  );
+}
